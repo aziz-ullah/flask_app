52bb642581747777643a832436ce88f12d9c95dd
Aziz Ullah <114052379+aziz-ullah@users.noreply.github.com>
2022-09-21 16:21:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import json
-from msilib import schema
-import re
-from unittest import result
 from flask import Flask,jsonify,request,render_template
 from flask_sqlalchemy import SQLAlchemy
 from webargs import fields,validate
