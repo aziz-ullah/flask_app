d62e2fc088aa50af5cab8301d3dc6efbdda551f2
Aziz Ullah <aziz.khan40343@gmail.com>
2022-09-21 15:50:32 +0500

    CRUD operaions

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask,jsonify,request
+import json
+from msilib import schema
+import re
+from unittest import result
+from flask import Flask,jsonify,request,render_template
 from flask_sqlalchemy import SQLAlchemy
 from webargs import fields,validate
 from webargs.flaskparser import use_args
@@ -36,7 +40,42 @@ def add_user(kwargs):
     except Exception as e:
         return 'User Not Added'
     
+@app.route('/user',methods = ["GET"])
+def get_all_users():
+    users = db.session.query(User).all()
+    return render_template('table.html',users = users)
 
+@app.route('/user',methods = ["PUT"])
+def update_user():
+    try:
+        user = db.session.query(User).filter(User.id == request.json["id"]).first()
+        if "username" in request.json:
+            user.username = request.json["username"]
+        
+        if "email" in request.json:
+            user.email = request.json["email"]
+            
+        db.session.commit()
+        return "User Updated"
+    except Exception as e:
+        print(e)
+        return "User not Updated"
+    
+@app.route('/user',methods = ["DELETE"])
+def delete_user():
+    try:
+        user = db.session.query(User).filter(User.id == request.json["id"]).first()
+        db.session.delete(user)
+        db.session.commit()
+        
+        return "User Deleted",200
+    except Exception as e:
+        print(e)
+        return "User not Deleted",422
+
+        
+
+    
 
 
 if __name__ == '__main__':
